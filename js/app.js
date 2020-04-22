02a419e9fa478a8e1abb58461e9e70784b154270
NOJEDNOPARTY <bordyuh@gmail.com>
2020-04-22 20:55:09 +0300

    add tamplate

---
+++ js/app.js
@@ -0,0 +1,28 @@
+var common = {
+	init: function() {
+		common.main();
+		common.owl();
+	},
+	main: function(){
+
+		// $('.menu-trigger').click(function(event){
+		// 	event.preventDefault();
+		// 	$('nav').addClass('open');
+		// })
+		// $('.nav-close').click(function(event){
+		// 	event.preventDefault();
+		// 	$('nav').removeClass('open');
+		// })
+		
+		// var bLazy = new Blazy({});
+
+
+	},
+	owl: function(){
+		
+	},
+};
+
+(function() {
+	common.init();
+}());
