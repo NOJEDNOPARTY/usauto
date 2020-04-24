9b27e342ef91eecda148f8df62a4e86dcc6c3939
NOJEDNOPARTY <bordyuh@gmail.com>
2020-04-25 01:34:04 +0300

    add some block of pc ver of website

--- js/app.js
+++ js/app.js
@@ -5,6 +5,10 @@ var common = {
 	},
 	main: function(){
 
+		$('.phone-mask').mask("+380 (99) 999-99-99");
+
+		new WOW().init();
+
 		// $('.menu-trigger').click(function(event){
 		// 	event.preventDefault();
 		// 	$('nav').addClass('open');
@@ -19,7 +23,58 @@ var common = {
 
 	},
 	owl: function(){
-		
+		$('.banner-slider').owlCarousel({
+			margin:0,
+			nav: true,
+			loop: true,
+			dots: false,
+			items: 1,
+			smartSpeed: 5000,
+			animateOut: 'fadeOut',
+			animateIn: 'fadeIn',
+		});
+
+
+		function bannerNav() {
+			$('.banner-slider').each(function(){
+				var product = $(this).find('.owl-item:not(.cloned)').length;
+
+				if(product > 8){
+					$(this).parent().find('.p-nav-all').text(product);
+
+					$(this).find('.owl-item:not(.cloned)').each(function(index, elem){
+						if($(this).hasClass('active')) {
+							var showProduct = index + 1;
+							$(this).closest('.banner').find('.p-nav-show').text(showProduct);
+						}
+					});
+				}else {
+					$(this).parent().find('.p-nav-all').text('0'+ product);
+
+					$(this).find('.owl-item:not(.cloned)').each(function(index, elem){
+						if($(this).hasClass('active')) {
+							var showProduct = index + 1;
+							$(this).closest('.banner').find('.p-nav-show').text('0'+ showProduct);
+						}
+					});
+				}
+
+			});
+		}
+
+		bannerNav();
+
+		$('.banner-slider .owl-nav button').click(function(){
+			bannerNav();
+			var bLazy = new Blazy({});
+		})
+
+		$('.banner-slider').on(' translated.owl.carousel', function(event) {
+			bannerNav();
+			var bLazy = new Blazy({});
+		})
+
+
 	},
 };
 
